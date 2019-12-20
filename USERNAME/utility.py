53e7d86f885587c0298e27377376e005f0701242
moneshs <moneshsundar19@gmail.com>
2019-12-20 05:18:45 -0500

    usernameprogram

---
+++ USERNAME/utility.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 05:03:03 2019
+
+@author: user
+"""
+
+def name(username,n):
+    if(n>3):
+        print("Hello "+ username + ",How are you")
+    else:
+        print("invalid name")
+        
