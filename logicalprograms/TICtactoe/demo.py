33c8b2eb9d787b8d61a615fc219a8523c7d5ab7e
moneshs <moneshsundar19@gmail.com>
2019-12-27 19:02:48 +0530

    [Monesh]ADD

---
+++ logicalprograms/TICtactoe/demo.py
@@ -0,0 +1,35 @@
+from utility import game_board,win
+import itertools
+play = True
+players = [1, 2]
+while play:
+    game = [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]]
+
+    game_won = False
+    player_cycle = itertools.cycle([1, 2])
+    game_board(game, just_display=True)
+    while not game_won:
+        current_player = next(player_cycle)
+        played = False
+        while not played:
+            print(f"Player: {current_player}")
+            column_choice = int(input("Which column? "))
+            row_choice = int(input("Which row? "))
+            played = game_board(game, player=current_player, row=row_choice, column=column_choice)
+
+        if win(game):
+            game_won = True
+            again = input("The game is over, would you like to play again? (y/n) ")
+            if again.lower() == "y":
+                print("restarting!")
+            elif again.lower() == "n":
+                print("exited")
+                play = False
+            else:
+                print("Not a valid answer")
+                play = False
+
+
+#game = game_board(game, player=1, row=2, column=1)
